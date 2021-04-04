53f1506b24ccfd6e6d378b795e99f0f46fb2443d
Christian Noll <christiannoll@gmx.net>
2021-04-04 17:59:06 +0200

    Initial Commit

---
+++ ThemeYourDay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThemeYourDay
+//
+//  Created by Christian on 04.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThemeYourDay/ThemeYourDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeYourDayApp.swift
+//  ThemeYourDay
+//
+//  Created by Christian on 04.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ThemeYourDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

88703ac696a5db503ef6921aa61411ac50de3db3
Christian Noll <christiannoll@gmx.net>
2021-04-05 14:05:45 +0200

    Added TextEditor

--- ThemeYourDay/ContentView.swift
+++ ThemeYourDay/ContentView.swift
@@ -1,16 +1,29 @@
-//
-//  ContentView.swift
-//  ThemeYourDay
-//
-//  Created by Christian on 04.04.21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var themetext = "Theme your day ..."
+    
+    init() {
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Spacer()
+            TextEditor(text: $themetext)
+                .padding()
+                .font(.largeTitle)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .disableAutocorrection(true)
+                .lineSpacing(20)
+            Spacer()
+        }
+        .background(Color.red)
+        .cornerRadius(25.0)
+        .frame(height: 400)
+        .padding()
     }
 }
 

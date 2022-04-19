bf6a000e6163e23cd1b8e21b49b5c199666e4bd4
Christian Noll <christiannoll@gmx.net>
2022-04-19 16:20:56 +0200

    Fix size of png image.

--- ThemeYourDay/Views/CanvasView.swift
+++ ThemeYourDay/Views/CanvasView.swift
@@ -55,7 +55,7 @@ struct CanvasView: View {
     
     private func savePngImage() {
         if canvasView.drawing.bounds.isEmpty == false {
-            let imgRect = CGRect(x: 0, y: 0, width: canvasView.drawing.bounds.width, height: canvasView.drawing.bounds.height)
+            let imgRect = CGRect(x: 0, y: 0, width: canvasView.bounds.height, height: canvasView.bounds.height)
             let image = canvasView.drawing.image(from: imgRect, scale: 1.0)
             if let data = image.pngData() {
                 modelData.savePngImageOfSelectedDay(data: data)

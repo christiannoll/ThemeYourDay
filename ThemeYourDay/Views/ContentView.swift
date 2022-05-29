9bf21e4a23f50d1971bc9da2548eec737f953102
Christian Noll <christiannoll@gmx.net>
2022-05-29 17:24:56 +0200

    Increased padding to mark today in CalendarView

--- ThemeYourDay/Views/ContentView.swift
+++ ThemeYourDay/Views/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                                     .foregroundColor(getCalendarForegroundColor(day))
                             )
                             .overlay(
-                                Circle().stroke(.white, lineWidth: modelData.isToday(day: day) ? 1 : 0).padding(3)
+                                Circle().stroke(.white, lineWidth: modelData.isToday(day: day) ? 1 : 0).padding(4)
                             )                    },
                     tag: "Calendar", selection: $selection) { EmptyView() }
                 

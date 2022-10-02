e48414b677fcda06850bf1b127a8269eece7e06a
Christian Noll <christiannoll@gmx.net>
2022-10-02 12:04:59 +0200

    Changed textLineSpacing in settings

--- ThemeYourDay/Model/Settings.swift
+++ ThemeYourDay/Model/Settings.swift
@@ -3,7 +3,7 @@ import Foundation
 struct Settings: Hashable, Codable {
     var fgColors: [DayColor]
     var bgColors: [DayColor]
-    var textLineSpacing = 20
+    var textLineSpacing = 10
     
     var weekdaysBgColor = Array(repeating: Day.defaultBgColor, count: 7)
     var weekdaysFgColor = Array(repeating: DayColor(), count: 7)

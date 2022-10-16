3e1c9f22407f63972a6027fd284866140eceace6
Christian Noll <christiannoll@gmx.net>
2022-10-16 14:16:08 +0200

    Init background color for ColorStripView in settings with five default colors.

--- ThemeYourDay/Model/Settings.swift
+++ ThemeYourDay/Model/Settings.swift
@@ -2,7 +2,11 @@ import Foundation
 
 struct Settings: Hashable, Codable {
     var fgColors = [DayColor()]
-    var bgColors = [Day.defaultBgColor]
+    var bgColors = [Day.defaultBgColor,
+                    DayColor(r: 249/255, g: 226/255, b: 49/255, a: 1.0),
+                    DayColor(r: 237/255, g: 35/255, b: 13/255, a: 1.0),
+                    DayColor(r: 96/255, g: 216/255, b: 56/255, a: 1.0),
+                    DayColor(r: 173/255, g: 173/255, b: 173/255, a: 1.0)]
     var textLineSpacing = 10
     
     var weekdaysBgColor = Array(repeating: Day.defaultBgColor, count: 7)

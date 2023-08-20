6c21474af0f21d7b1608c5643424312dd0308ff4
Christian Noll <christiannoll@gmx.net>
2023-08-20 15:42:41 +0200

    Fix TextAlignmentView

--- ThemeYourDay/Views/TextAlignmentView.swift
+++ ThemeYourDay/Views/TextAlignmentView.swift
@@ -2,20 +2,23 @@ import SwiftUI
 
 struct TextAlignmentView: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var alignment: MyTextAlignment = MyTextAlignment.center
     
     var body: some View {
         HStack {
             /*Label("Align:", systemImage: "textformat.alignleft")
                 .labelStyle(.titleOnly)
                 .font(.subheadline)*/
-            Picker("Text alignment", selection: $modelData.selectedDay.textAlignment) {
+            Picker("Text alignment", selection: $alignment) {
                 Image(systemName: "text.alignleft").tag(MyTextAlignment.leading)
                 Image(systemName: "text.aligncenter").tag(MyTextAlignment.center)
                 Image(systemName: "text.alignright").tag(MyTextAlignment.trailing)
             }
             .pickerStyle(.segmented)
-            .onChange(of: modelData.selectedDay.textAlignment) {
-                modelData.save()
+            .onChange(of: alignment) {
+                if let selectedDay = modelData.selectedMyDay {
+                    selectedDay.textAlignment = alignment
+                }
             }
         }
     }

28ddefba8b02a77c96d380766e8d5ac6494026cf
Christian Noll <christiannoll@gmx.net>
2025-11-18 17:39:03 +0100

    Updated deployment target to iOS26

--- ThemeYourDay/Views/CarouselView.swift
+++ ThemeYourDay/Views/CarouselView.swift
@@ -61,9 +61,29 @@ struct CarouselView: View {
 
 extension CGFloat {
     static var screenWidth: CGFloat {
-        UIScreen.main.bounds.width
+        UIScreen.current?.bounds.width ?? 0
     }
 }
+
+extension UIWindow {
+    static var current: UIWindow? {
+        for scene in UIApplication.shared.connectedScenes {
+            guard let windowScene = scene as? UIWindowScene else { continue }
+            for window in windowScene.windows {
+                if window.isKeyWindow { return window }
+            }
+        }
+        return nil
+    }
+}
+
+
+extension UIScreen {
+    static var current: UIScreen? {
+        UIWindow.current?.screen
+    }
+}
+
 
 #Preview {
     CarouselView()

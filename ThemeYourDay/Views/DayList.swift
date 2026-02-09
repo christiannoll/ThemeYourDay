ede1c6bc17ac6fb64e15ea68d2eb90ef496a3f8b
Christian Noll <christiannoll@gmx.net>
2026-02-09 18:28:51 +0100

    Use searchable modifier for search in DayList

--- ThemeYourDay/Views/DayList.swift
+++ ThemeYourDay/Views/DayList.swift
@@ -6,8 +6,7 @@ struct DayList: View {
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     @State var showDeleteAlert = false
     @State private var query = ""
-    @State private var searching = false
-    
+
     @Query(sort: [SortDescriptor(\Day.id)]) private var days: [Day]
     @Query() var settings: [Settings]
     
@@ -19,52 +18,49 @@ struct DayList: View {
     }
     
     var body: some View {
-        VStack {
-            if searching {
-                SearchBar(searchText: $query, searching: $searching)
-            }
-            ScrollViewReader { proxy in
-                List(filteredDays) {
-                    let day = $0
-                    DayListCell(day: day, isToday: modelData.isToday(day: day))
-                        .onTapGesture {
-                            modelData.selectDay(day, days: days)
-                            self.mode.wrappedValue.dismiss()
-                        }
-                        .padding(.vertical, 8)
-                        .id(day)
-                        .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                        .listRowSeparator(.hidden)
-                }
-                .scrollContentBackground(.hidden)
-                .onAppear {
-                    DispatchQueue.main.async {
-                        proxy.scrollTo(modelData.getToday(days), anchor: .center)
+        ScrollViewReader { proxy in
+            List(filteredDays) {
+                let day = $0
+                DayListCell(day: day, isToday: modelData.isToday(day: day))
+                    .onTapGesture {
+                        modelData.selectDay(day, days: days)
+                        self.mode.wrappedValue.dismiss()
                     }
+                    .padding(.vertical, 8)
+                    .id(day)
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .listRowSeparator(.hidden)
+            }
+            .scrollContentBackground(.hidden)
+            .onAppear {
+                DispatchQueue.main.async {
+                    proxy.scrollTo(modelData.getToday(days), anchor: .center)
                 }
-                .onChange(of: query) {
-                    if query.isEmpty {
-                        if let selectedDay = modelData.selectedDay {
-                            proxy.scrollTo(selectedDay, anchor: .center)
-                        }
+            }
+            .onChange(of: query) {
+                if query.isEmpty {
+                    if let selectedDay = modelData.selectedDay {
+                        proxy.scrollTo(selectedDay, anchor: .center)
                     }
                 }
             }
-            .navigationBarTitle("Days", displayMode: .inline)
-            .navigationBarItems(trailing: Menu {
-                Button(action: { showDeleteAlert.toggle()}) {
-                    Label("RemoveThemes", systemImage: "trash")
+        }
+        .searchable(text: $query, placement: .toolbar, prompt: "Search")
+        .navigationTitle("Days")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("menu", systemImage: "ellipsis") {
+                    Button(action: { showDeleteAlert.toggle()}) {
+                        Label("RemoveThemes", systemImage: "trash")
+                    }
+                    Button(action: { shareCsvFile()}) {
+                        Label("ExportThemes", systemImage: "square.and.arrow.up")
+                    }
                 }
-                Button(action: { shareCsvFile()}) {
-                    Label("ExportThemes", systemImage: "square.and.arrow.up")
-                }
-            } label: { Image(systemName: "ellipsis.circle") })
-            .navigationBarItems(trailing: Button(action: { withAnimation {searching.toggle() }} ) {
-                Image(systemName: "magnifyingglass")
-            })
-            .alert(isPresented: $showDeleteAlert) {
-                Alert(title: Text("Remove All Themes?"), message: Text("This will remove all themes and can't be undone."), primaryButton: .destructive(Text("Remove"), action: { removeAllDays() }), secondaryButton: .cancel(Text("Cancel")))
             }
+        }
+        .alert(isPresented: $showDeleteAlert) {
+            Alert(title: Text("Remove All Themes?"), message: Text("This will remove all themes and can't be undone."), primaryButton: .destructive(Text("Remove"), action: { removeAllDays() }), secondaryButton: .cancel(Text("Cancel")))
         }
     }
     

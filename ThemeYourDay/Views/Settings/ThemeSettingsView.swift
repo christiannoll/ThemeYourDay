c248e35145dad0a71b72e38391a825ec4f63a1b2
Christian Noll <christiannoll@gmx.net>
2026-03-08 08:59:36 +0100

    Updated ThemeSettingsView

--- ThemeYourDay/Views/Settings/ThemeSettingsView.swift
+++ ThemeYourDay/Views/Settings/ThemeSettingsView.swift
@@ -3,11 +3,12 @@ import SwiftUI
 struct ThemeSettingsView: View {
     var body: some View {
         Form {
-            Section(header: Text("ThemeTemplate")) {
+            Section {
                 NavigationLink("BackgroundColors", destination: WeekSettingsView(weekSettingsType: .bgcolor))
                 NavigationLink("ForegroundColors", destination: WeekSettingsView(weekSettingsType: .fgcolor))
                 NavigationLink("Texts", destination: WeekSettingsView(weekSettingsType: .text))
             }
         }
+        .navigationBarTitle("ThemeTemplate")
     }
 }

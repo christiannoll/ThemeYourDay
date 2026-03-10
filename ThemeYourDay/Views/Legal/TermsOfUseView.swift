b7b4817ae8441fe6e54a2d80f41e5ff7552d233b
Christian Noll <christiannoll@gmx.net>
2026-03-10 17:49:43 +0100

    Simplified TermsOfUseView

--- ThemeYourDay/Views/Legal/TermsOfUseView.swift
+++ ThemeYourDay/Views/Legal/TermsOfUseView.swift
@@ -3,11 +3,10 @@ import SwiftUI
 struct TermsOfUseView: View {
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Die Nutzung der App ThemeYourDay unterliegt Apples Standard-EULA für Apps. Die in ThemeYourDay bereitgestellten Trinkgelder schalten keine zusätzlichen Inhalte in der App frei.")
-                .padding()
-            Spacer()
+        Form {
+            Section("Nutzungsbedingungen") {
+                Text("Die Nutzung der App ThemeYourDay unterliegt Apples Standard-EULA für Apps. Die in ThemeYourDay bereitgestellten Trinkgelder schalten keine zusätzlichen Inhalte in der App frei.")
+            }
         }
-        .navigationTitle("Nutzungsbedingungen")
     }
 }
